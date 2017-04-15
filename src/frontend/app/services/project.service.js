5b6d89d1f776c291aa2a5a889e83d4461d948caf
toledoJavier <toledojavier90@gmail.com>
2017-04-14 21:13:47 -0300

    agrego los servicios de proyecto

---
+++ src/frontend/app/services/project.service.js
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export default class ProjectService {
+
+	constructor(http) {
+      this.http = http
+      this._projects = []
+      this.http.get("/proyectos").toPromise()
+              .then(response => this._projects.push(...response.json()))
+              .catch(err => console.log(err))
+    }
+
+    get projects() {
+      return this._projects
+    }
+    
+    getProject(id) {
+      return this.http.get(`/proyectos/${id}`).toPromise()
+              .then(response => response.json());
+    }
+
+    create(project) {
+      this.http.post("/proyectos", JSON.stringify(project), { headers:{'Content-Type': 'application/json'}})
+              .toPromise()
+              .then(response => this._projects.push(project))
+              .then(console.log(project))
+              .catch(err => console.log(err))
+    }
+}
+
+ProjectService.parameters = [
+  Http
+]
