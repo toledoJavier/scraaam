9bb98539cc6be5c9a068fa83d0cd2a405536d247
toledoJavier <toledojavier90@gmail.com>
2017-06-04 12:53:48 -0300

    Tarea para levantar el backend con watch y reload

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,19 @@
 import gulp from 'gulp'
 import gutil from 'gulp-util'
+import babel from 'gulp-babel'
+import gls from 'gulp-live-server'
 
-gulp.task('default', () => {
-	return gutil.log('Gulp is running!')
+gulp.task('transpile', () => {
+	return gulp.src(['src/backend/**/*.js'])
+		.pipe(babel())
+		.pipe(gulp.dest('dist/backend/'))
+})
+
+gulp.task('start:watch', ['transpile'], () => {
+	const server = gls.new('dist/backend/server.js')
+	server.start()
+
+	gulp.watch('src/**/*.js', ['transpile', () => {		
+		server.start()
+	}])
 })
