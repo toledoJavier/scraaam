af8462655c19b943c643edbaf6e8a7b330765fe3
toledoJavier <toledojavier90@gmail.com>
2017-06-04 16:00:41 -0300

    Tarea para correr los tests del backend

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,6 +2,7 @@ import gulp from 'gulp'
 import gutil from 'gulp-util'
 import babel from 'gulp-babel'
 import gls from 'gulp-live-server'
+import mocha from 'gulp-mocha'
 
 gulp.task('transpile', () => {
 	return gulp.src(['src/backend/**/*.js'])
@@ -16,4 +17,14 @@ gulp.task('start:watch', ['transpile'], () => {
 	gulp.watch('src/**/*.js', ['transpile', () => {		
 		server.start()
 	}])
+})
+
+gulp.task('backend', () => {
+	gulp.src('test/backend/**/*.js', {read: false})
+		.pipe(mocha({
+			compilers: 'js:babel-core/register',
+			timeout: 120000,
+			globals: ['recursive'],
+			require: ['babel-polyfill']
+		}))
 })
