fe68a69a48a56120e9c9003fb776a7454438b317
toledoJavier <toledojavier90@gmail.com>
2017-07-04 16:01:27 -0300

    Script para obtener el nombre del repo de dockerhub

---
+++ dockerhub.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+repoName() {
+	local DOCKERHUB_REPO_NAME=""
+	if [ "$TRAVIS_BRANCH" == "master" ]; then
+		local DOCKERHUB_REPO_NAME="stable"
+     elif [ "$TRAVIS_BRANCH" == "development" ]; then
+     	local DOCKERHUB_REPO_NAME="devel"
+    fi
+    echo ${DOCKERHUB_REPO_NAME}
+}
