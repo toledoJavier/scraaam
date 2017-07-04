620a27868d6962e95087a2c9a2f527bda8bb4ea0
toledoJavier <toledojavier90@gmail.com>
2017-07-04 16:12:10 -0300

    fix

--- dockerhub.sh
+++ dockerhub.sh
@@ -4,8 +4,8 @@ repoName() {
 	local DOCKERHUB_REPO_NAME=""
 	if [ "$TRAVIS_BRANCH" == "master" ]; then
 		local DOCKERHUB_REPO_NAME="stable"
-     elif [ "$TRAVIS_BRANCH" == "development" ]; then
-     	local DOCKERHUB_REPO_NAME="devel"
-    fi
-    echo ${DOCKERHUB_REPO_NAME}
+  elif [ "$TRAVIS_BRANCH" == "development" ]; then
+  	local DOCKERHUB_REPO_NAME="devel"
+  fi;
+  echo ${DOCKERHUB_REPO_NAME}
 }
